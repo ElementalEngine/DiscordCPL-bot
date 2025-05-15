9b8cae414b99c7fe8e615b3211e3317ab0a29a36
ProjectCisco <sga.accounts@proton.me>
2025-05-15 23:25:15 +0100

    TODOO: implement Civ7DraftService class for managing Civ7 draft logic

--- src/commands/votes/civ7draftvote.ts
+++ src/commands/votes/civ7draftvote.ts
@@ -1,45 +1,45 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
-import { config } from '../../config';
-import { addOptionalMentions, ensureChannel, ensurePermissions } from '../../utils';
-import { VoteService } from '../../services/votes';
+// import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
+// import { config } from '../../config';
+// import { addOptionalMentions, ensureChannel, ensurePermissions } from '../../utils';
+// import { VoteService } from '../../services/votes';
 
-export const data = addOptionalMentions(
-  new SlashCommandBuilder()
-    .setName('civ7draft')
-    .setDescription('Initiate a Civ7 draft vote')
-    .addStringOption(option =>
-      option
-        .setName('gamemode')
-        .setDescription('Select game mode: FFA or Team')
-        .setRequired(true)
-        .addChoices(
-          { name: 'ffa',   value: 'ffa'   },
-          { name: 'team',  value: 'team'  }
-        )
-    )
-    .addStringOption(option =>
-      option
-        .setName('startingage')
-        .setDescription('Select starting age')
-        .setRequired(true)
-        .addChoices(
-          { name: 'Antiquity',  value: 'Antiquity_Age'   },
-          { name: 'Exploration', value: 'Exploration_Age' },
-          { name: 'Modern',      value: 'Modern_Age'      }
-        )
-    ) as SlashCommandBuilder
-);
+// export const data = addOptionalMentions(
+//   new SlashCommandBuilder()
+//     .setName('civ7draft')
+//     .setDescription('Initiate a Civ7 draft vote')
+//     .addStringOption(option =>
+//       option
+//         .setName('gamemode')
+//         .setDescription('Select game mode: FFA or Team')
+//         .setRequired(true)
+//         .addChoices(
+//           { name: 'ffa',   value: 'ffa'   },
+//           { name: 'team',  value: 'team'  }
+//         )
+//     )
+//     .addStringOption(option =>
+//       option
+//         .setName('startingage')
+//         .setDescription('Select starting age')
+//         .setRequired(true)
+//         .addChoices(
+//           { name: 'Antiquity',  value: 'Antiquity_Age'   },
+//           { name: 'Exploration', value: 'Exploration_Age' },
+//           { name: 'Modern',      value: 'Modern_Age'      }
+//         )
+//     ) as SlashCommandBuilder
+// );
 
-export async function execute(interaction: ChatInputCommandInteraction) {
-  const gamemode = interaction.options.getString('gamemode', true);
-  const allowedChannelId =
-    gamemode === 'ffa'
-      ? config.discord.channels.civ7ffavoting
-      : config.discord.channels.civ7teamvoting;
+// export async function execute(interaction: ChatInputCommandInteraction) {
+//   const gamemode = interaction.options.getString('gamemode', true);
+//   const allowedChannelId =
+//     gamemode === 'ffa'
+//       ? config.discord.channels.civ7ffavoting
+//       : config.discord.channels.civ7teamvoting;
 
-  if (!ensureChannel(interaction, [allowedChannelId])) return;
-  if (!ensurePermissions(interaction, [config.discord.roles.Civ7Rank])) return;
+//   if (!ensureChannel(interaction, [allowedChannelId])) return;
+//   if (!ensurePermissions(interaction, [config.discord.roles.Civ7Rank])) return;
 
-  await VoteService.civ7Draft(interaction);
-}
+//   await VoteService.civ7Draft(interaction);
+// }
 
---
+++ src/services/votes/civ7-draft.service.ts
@@ -0,0 +1,5 @@
+import { Client } from 'discord.js';
+
+export default class Civ7DraftService {
+  constructor(private client: Client) {}
+}
